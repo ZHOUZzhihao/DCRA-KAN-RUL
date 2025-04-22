6b24605462db1110497fdfbfaeb5f91f59d0f812
ZhouZhihao <115009587+ZHOUZzhihao@users.noreply.github.com>
2025-04-22 18:42:26 +0800

    Update model_DCRAKAN.py

--- model_DCRAKAN.py
+++ model_DCRAKAN.py
@@ -7,22 +7,30 @@ from torchvision.ops import DeformConv2d
 
 #TODO:  Proposed Deformable Convolutional Residual Attention Module
 class InvertedResidual(nn.Module):
-  
+        ""
+        The program will be uploaded as soon as the article is published
+        ""
         return ()
 #TODO: KAN Module
 class KANLinear(torch.nn.Module):
-   
+        ""
+        The program will be uploaded as soon as the article is published
+        ""
         return ()
 
 #TODO: Positional coding: making the network capable of temporal learning
 class FixedPositionalEncoding(nn.Module):
-    
+        ""
+        The program will be uploaded as soon as the article is published
+        ""
         return x.transpose(0, 1)
 
 
 #TODO: DCRA enhanced KAN model
 class DCNNKAN(nn.Module):
-  
+        ""
+        The program will be uploaded as soon as the article is published
+        ""
         return x
 
 def print_parameter_details(model):
